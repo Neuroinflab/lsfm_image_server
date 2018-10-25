c6504bcd583560eac9dd3e687f826a849eefc6f0
Sylwia Bednarek <s.bednarek@nencki.gov.pl>
2018-10-25 16:15:31 +0200

    formatting

--- src/lmdf.py
+++ src/lmdf.py
@@ -47,7 +47,9 @@ SIGN_LPI_RAS_T = np.array([1, 1, -1])
 SIGN_LPI_RAS_A = np.array([[-1, 1, -1], [1, -1, -1], [1, 1, -1]])
 
 SIGN_RAS_T = np.array([-1, -1, -1])
-SIGN_RAS_A = np.array([[-1, 1, -1], [1, -1, -1], [1, 1, -1]])
+SIGN_RAS_A = np.array([[-1, 1, -1],
+                       [1, -1, -1],
+                       [1, 1, -1]])
 DIRECTION_RAS = (1.0, 0.0, 0.0,
                  0.0, 1.0, 0.0,
                  0.0, 0.0, -1.0)
