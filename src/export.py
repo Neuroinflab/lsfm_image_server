b5355dcdb20f5f4d10349b8be64ecb64d858cb8e
Sylwia Bednarek <s.bednarek@nencki.gov.pl>
2018-11-06 16:08:26 +0100

    added licence boilerplate to modules

--- src/export.py
+++ src/export.py
@@ -1,3 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+###############################################################################
+#                                                                             #
+#    LSFM image server                                                        #
+#                                                                             #
+#    Copyright (C) 2017-2018 Sylwia Bednarek, Piotr Majka                     #
+#    (Laboratory of Neuroinformatics; Nencki Institute of Experimental        #
+#    Biology of Polish Academy of Sciences)                                   #
+#                                                                             #
+#    This software is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This software is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this software.  If not, see http://www.gnu.org/licenses/.     #
+#                                                                             #
+###############################################################################
+
 import os
 import logging
 import processing
