ef1acc34f50ca08bb77de0578e6ddd07f9efe447
s-bednarek <40997153+s-bednarek@users.noreply.github.com>
2023-04-21 13:27:48 +0200

    Add files via upload
    Added script for automating the process of downloading data

---
+++ step_00_download_data.sh
@@ -0,0 +1,18 @@
+curl 'https://repod.icm.edu.pl/api/access/datafile/17622' --output cfos_channel_part_1.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17616' --output cfos_channel_part_2.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17615' --output cfos_channel_part_3.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17621' --output cfos_channel_part_4.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17619' --output cfos_channel_part_5.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17617' --output cfos_channel_part_6.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17620' --output cfos_channel_part_7.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17618' --output cfos_channel_part_8.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17623' --output cfos_channel_part_9.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17624' --output cfos_channel_part_10.zip
+curl 'https://repod.icm.edu.pl/api/access/datafile/17625' --output auto_channel_part_1.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17626' --output auto_channel_part_2.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17628' --output auto_channel_part_3.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17629' --output auto_channel_part_4.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17627' --output auto_channel_part_5.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17632' --output auto_channel_part_6.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17630' --output auto_channel_part_7.zip -: \
+'https://repod.icm.edu.pl/api/access/datafile/17631' --output auto_channel_part_8.zip
